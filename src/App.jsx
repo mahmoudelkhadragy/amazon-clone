6307eb07b25da82a28cf74b65437615783ff1c4b
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-09-25 02:42:20 +0200

    add login

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Checkout from "./components/checkout/Checkout";
+import Login from "./components/login/Login";
+import { auth } from "./firebase";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useStateValue } from "./context/StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will oly run once when the app component loads.
+    auth.onAuthStateChanged((authUser) => {
+      console.log("the user is >> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBiwWnyDtiHMvoq3z7ndd46BPuKQaTFBzw",
   authDomain: "clone-1c51d.firebaseapp.com",
@@ -6,5 +8,12 @@ const firebaseConfig = {
   storageBucket: "clone-1c51d.appspot.com",
   messagingSenderId: "2969359527",
   appId: "1:2969359527:web:afa86ba068b3111a8ab782",
-  measurementId: "G-FFCZ2FJE3C"
+  measurementId: "G-FFCZ2FJE3C",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
